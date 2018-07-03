c33abce717cd08aaf0a07377ba0c535f99651a54
Akira Hasegawa <akirahasegawa@akiras-macbook-pro.local>
2018-07-03 15:54:06 -0700

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Akira Hasegawa on 7/3/18.
+//  Copyright © 2018 Akira Hasegawa. All rights reserved.
+//
+
+import Foundation
+
+
+class Item  {
+    var title : String = ""
+    var done : Bool = false
+    
+}
